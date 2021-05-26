752c44a781017f51ab58ac14cac5b3c748de24ac
VladimirGavrilov <gavriloff.v@yandex.ru>
2021-05-26 10:09:31 +0300

    2+3

--- src/App.js
+++ src/App.js
@@ -7,49 +7,52 @@ import './App.css';
 function App(props) {
   return (
   <div className='App'>
-      <header className='App-header'>
-      <h2>
-         TOP LINE
+    <div className='wrapper'>
+        <header className='App-header'>
+          <h2>
+            TOP LINE
       </h2>
-    </header>
-    <aside className='App-content-right'>
-      <nav>
-        <ul>
-          <li>
-            <h5>melu p 4</h5>
-          </li>
-        </ul>
-      </nav>
-    </aside>
-      <main className='App-content-left'>
-        <section>
-          <h1>Съёмки фильма Полипропилен</h1>
-          <p>
-              История о том, как снимали фильм, где герои отдыхали на пляже, потом пришёл антагонист, избил протагонистов, бросил их в бассейн, и что из этого получилось.
-          </p>
-        </section>
-        <section>
-        <h1>Хороший язык</h1>
-          <p>
-            История о том, как проходила студия изучения языка эсперанто, в то время, как над ней, на веранде велась студия приколистов, где травились анекдоты, и что из этого получилось.
-          </p>
-        </section>
-        <section>
-          <h1>Хороший язык2</h1>
-          <p>
-            История о том, как проходила студия изучения языка эсперанто, в то время, как над ней, на веранде велась студия приколистов, где травились анекдоты, и что из этого получилось.
-          </p>
-        </section>
-        <article>
-          <h1>Article</h1>
-            История о том, как возле столовой появились загадочные розовые
-            следы с шестью пальцами, и почему это случилось.
-        </article>
-      </main>
-      <footer className='App-footer'>
-        Copyright Gavrilov Wladimir
+        </header>
+        <div className='App-content'>
+          <aside className='App-content-right'>
+            <nav>
+              <ul>
+                <li><h5>melu p 4</h5></li>
+              </ul>
+            </nav>
+          </aside>
+          <main className='App-content-left'>
+            <div className='App-content-on_and_two'>
+              <div className='App-content-2col'>
+                <section>
+                  <h1>Съёмки фильма Полипропилен</h1>
+                  <p>
+                    История о том, как снимали фильм, где герои отдыхали на пляже, потом пришёл антагонист, избил протагонистов, бросил их в бассейн, и что из этого получилось.
+                  </p>
+                </section>
+              </div>
+              <div className='App-content-2row'>
+                <section>
+                  <h1>Хороший язык</h1>
+                  <p>
+                    История о том, как проходила студия изучения языка эсперанто, в то время, как над ней, на веранде велась студия приколистов, где травились анекдоты, и что из этого получилось.
+                      </p>
+                </section>
+                <section>
+                  <h1>Хороший язык2</h1>
+                  <p>
+                    История о том, как проходила студия изучения языка эсперанто, в то время, как над ней, на веранде велась студия приколистов, где травились анекдоты, и что из этого получилось.
+                      </p>
+                </section>
+              </div>
+            </div>   
+          </main> 
+        </div>        
+        <footer className='App-footer'>
+          Copyright Gavrilov Wladimir
       </footer>
-    </div>
+    </div>    
+  </div>
   );
 }
 
