c9c93d72bfe5a3d4bb8e5140126643e2a27f3031
VladimirGavrilov <gavriloff.v@yandex.ru>
2021-05-26 09:06:23 +0300

    n 1

--- src/App.js
+++ src/App.js
@@ -1,76 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import './App.css';
 
-function App() {
+
+function App(props) {
   return (
-    <div className="Wrapper">
-      <header className="HeaderWr">
-        <div>
-          Верхняя синяя линия!
-        </div>
-        <nav>
-          <button><a href="#">Об учреждении</a></button>
-          <button><a href="#">Новости</a></button>  
-          <button> <a href="#">Контакты</a></button>
-          <button><a href="#">Направления деятельности</a></button>                   
-        </nav> 
-      </header>
-      <section>
-        <div>LOGO</div>
-        <aside>
-          <a href="#">МОЦ</a>
-          <a href="#">Постановка на учет в ДОУ</a>
-          <a href="#">Отдых летний</a>
-          <a href="#">Трудоустройство несовершеннолетних</a>
-        </aside>
-
-      </section>
-      <section>
-        <div>
-          <h3>Новости</h3>
-          <div className="leftNews">
-            Left
-          </div>
-          <div className="rigftNews">
-            <div>
-              RigftTop
-            </div>
-            <div>
-              RigftBottom
-            </div>
-          </div>
-
-        </div>
-      </section>
-      <footer>
-        <div>
-          <h3>
-            Полезные ссылки
-          </h3>
-          <div className="bottomCards">
-            <div className="cardsOne">
-              <a href=""><img src="./img/1.jpg" alt="minObr" /></a>
-              <a href=""><img src="./img/6.jpg" alt="minObr" /></a>
-              <a href=""><img src="./img/9.jpg" alt="minObr" /></a>
-              <a href=""><img src="./img/1.jpg" alt="minObr" /></a>
-              <a href=""><img src="./img/6.jpg" alt="minObr" /></a>
-              <a href=""><img src="./img/9.jpg" alt="minObr" /></a>
-              <a href=""><img src="./img/1.jpg" alt="minObr" /></a>
-              <a href=""><img src="./img/6.jpg" alt="minObr" /></a>
-              <a href=""><img src="./img/9.jpg" alt="minObr" /></a>
-              <a href=""><img src="./img/1.jpg" alt="minObr" /></a>
-              <a href=""><img src="./img/6.jpg" alt="minObr" /></a>
-              <a href=""><img src="./img/9.jpg" alt="minObr" /></a>
-              
-             
-
-            </div>
-
-          </div>
-        </div>
-
+  <div className='App'>
+      <header className='App-header'>
+      <h2>
+         TOP LINE
+      </h2>
+    </header>
+    <aside className='App-content-right'>
+      <nav>
+        <ul>
+          <li>
+            <h5>melu p 4</h5>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+      <main className='App-content-left'>
+        <section>
+          <h1>Съёмки фильма Полипропилен</h1>
+          <p>
+              История о том, как снимали фильм, где герои отдыхали на пляже, потом пришёл антагонист, избил протагонистов, бросил их в бассейн, и что из этого получилось.
+          </p>
+        </section>
+        <section>
+        <h1>Хороший язык</h1>
+          <p>
+            История о том, как проходила студия изучения языка эсперанто, в то время, как над ней, на веранде велась студия приколистов, где травились анекдоты, и что из этого получилось.
+          </p>
+        </section>
+        <section>
+          <h1>Хороший язык2</h1>
+          <p>
+            История о том, как проходила студия изучения языка эсперанто, в то время, как над ней, на веранде велась студия приколистов, где травились анекдоты, и что из этого получилось.
+          </p>
+        </section>
+        <article>
+          <h1>Article</h1>
+            История о том, как возле столовой появились загадочные розовые
+            следы с шестью пальцами, и почему это случилось.
+        </article>
+      </main>
+      <footer className='App-footer'>
+        Copyright Gavrilov Wladimir
       </footer>
-
     </div>
   );
 }
