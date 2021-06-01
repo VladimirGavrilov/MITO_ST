b4d6abdfa076b4a0c58628512f5d8ae7213f9e8a
VladimirGavrilov <gavriloff.v@yandex.ru>
2021-06-01 17:07:19 +0300

    add component navv

--- src/AppNav/AppNav.js
+++ src/AppNav/AppNav.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Link } from "react-router-dom";
-
+// function addLi(props) {
+//   return(
+//     <li><a href={props.hrefNavLink}><h5>{props.nameNavLink}</h5></a></li>
+//   )
+// }
 
  function AppNav(props) {
     const menuNavLinks = props.NavLinks;
-    const navLinks = menuNavLinks.map( item => <li><a href={item.hrefNavLink}><h5>{item.nameNavLink}</h5></a></li>)
+    const navLinks = menuNavLinks.map( (item) => <li><a href={item.hrefNavLink}><h5>{item.nameNavLink}</h5></a></li> ) 
   return(
         <nav className= {props.navClass}>            
-            <ul>{ navLinks}</ul>
+            <ul>{navLinks}</ul>
         </nav>
   )
 }
